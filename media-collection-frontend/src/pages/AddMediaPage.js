cee3a2bfd72cb9339b1270219c46d73c3ecfc547
ELBEK-9 <khaydarovelbek@gmail.com>
2025-04-16 20:22:32 +0500

    now adding, deleting, filtering and searching works without any bugs. I am also using local storage just so because we don't have backend yet

--- media-collection-frontend/src/pages/AddMediaPage.js
+++ media-collection-frontend/src/pages/AddMediaPage.js
@@ -5,15 +5,23 @@ import MediaForm from '../components/MediaForm';
 const AddMediaPage = () => {
   const navigate = useNavigate();
 
+//  const handleCreate = async (data) => {
+//    try {
+////    USING CONSOLE BECAUSE NO BACKEND
+////      await createMedia(data);
+//      console.log('Creating media:', data);
+//      navigate('/'); // Go back to home
+//    } catch (err) {
+//      console.error('Error creating media:', err);
+//    }
+//  };
+
   const handleCreate = async (data) => {
-    try {
-//    USING CONSOLE BECAUSE NO BACKEND
-//      await createMedia(data);
-      console.log('Creating media:', data);
-      navigate('/'); // Go back to home
-    } catch (err) {
-      console.error('Error creating media:', err);
-    }
+    const existing = JSON.parse(localStorage.getItem('media')) || [];
+    const newItem = { ...data, id: Date.now() }; // add a unique ID
+    const updated = [...existing, newItem];
+    localStorage.setItem('media', JSON.stringify(updated));
+    navigate('/');
   };
 
   return (
