ac0c28ce4c5d6381db423a78d79b0fe174a29759
Bekmurod O'ktamov <bekmurod2510@gmail.com>
2025-04-20 19:08:04 +0300

    Navbar frontend fixed
    Added styling, and couples of buttons. Now time to add functionality

--- media-collection-frontend/src/components/Navbar.js
+++ media-collection-frontend/src/components/Navbar.js
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav style={{ padding: "1rem", background: "#f3f3f3" }}>
-      <Link to="/">Home</Link> |
-      <Link to="/add"> Add Media</Link>
+    <nav className="navbar" style={{ padding: "1rem", background: "#f3f3f3" }}>
+    <div className="container">
+      <div className="container-right">
+        <Link to="/"><button className="Register Home">Home</button></Link>
+        </div>
+        <div className="container-left">
+          <Link to="/add" className="AddMediaBtn"> Add Media</Link>
+          <Link to="">
+            <button className="Register Login">Login</button>
+          </Link>
+          <Link to="">
+            <button className="Register Signup">SingUp</button>
+          </Link>
+        </div>
+    </div>
     </nav>
-  );
+);
 };
 
 export default Navbar;
